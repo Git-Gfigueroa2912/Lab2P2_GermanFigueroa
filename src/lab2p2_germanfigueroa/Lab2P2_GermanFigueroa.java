5152d48723de0c0d386446ed4dde693ff9f77c46
Git-Gfigueroa2912 <german_figueroa@unitec.edu>
2026-01-30 12:46:23 -0600

    Laboratorio 2

---
+++ src/lab2p2_germanfigueroa/Lab2P2_GermanFigueroa.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package lab2p2_germanfigueroa;
+
+/**
+ *
+ * @author gafm2
+ */
+public class Lab2P2_GermanFigueroa {
+
+    
+    public static void main(String[] args) {
+        
+    }
+    
+}
